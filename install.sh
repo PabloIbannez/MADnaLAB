1b6b969806023d30e942cd809e69f4c4b8383c61
Pablo Ibanez <p.ibanez.fre@gmail.com>
2022-03-17 09:40:23 +0100

    Required python packages are now added with install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ BIN_path="/home/pablo/bin"
 UAMMD_path="/home/pablo/Desktop/UAMMD"
 
 python ./src/setENV.py $BIN_path $UAMMD_path
-
 source ~/.bashrc
+
+python -m pip install -r requirements.txt
 
 make
 cp ./bin/MADnaLAB $BIN_path
